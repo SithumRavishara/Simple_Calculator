11e69b09740db0c56b668dde6908944aaf8fc486
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2018-08-21 11:58:18 +0530

    update files

---
+++ V1.0/src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,16 @@
+
+package simplecalculator;
+
+/**
+ *
+ * @author Sithum Ravishara
+ */
+public class SimpleCalculator {
+
+    
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
+    }
+    
+}
